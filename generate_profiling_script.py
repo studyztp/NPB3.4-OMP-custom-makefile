ea4532954ee56b28ee729a9c984211cb29706b78
studyztp <studyztp@gmail.com>
2024-06-14 11:18:02 -0700

    fix the broken profiling script genereator

--- generate_profiling_script.py
+++ generate_profiling_script.py
@@ -2,6 +2,7 @@ from pathlib import Path
 
 benchmarks = ["bt", "cg", "ep", "ft", "is", "lu", "mg", "sp"]
 size = "B"
+region_size = 100000000
 
 current_location = Path.cwd()
 
@@ -19,21 +20,6 @@ x86_64_environment_setup = f"export LD_LIBRARY_PATH={current_location}/common/" 
     + f"{current_location}/common/all_papi/x86_64/lib:$LD_LIBRARY_PATH;\n" \
     + "export OMP_NUM_THREADS=1;"
 
-with output_bash_aarch64_script.open("w") as f:
-    with output_bash_x86_64_script.open("w") as f2:
-        with diff_script.open("w") as f3:
-            f.write(aarch64_environment_setup + "\n")
-            f2.write(x86_64_environment_setup + "\n")
-            for bench in benchmarks:
-                f.write(f"make clean_all PROGRAM={bench};\n")
-                f.write(f"make profiling PROGRAM={bench} SIZE={size} TARGET_ARCH=aarch64;\n")
-                f2.write(f"make cross_compile_profiling PROGRAM={bench} SIZE={size} TARGET_ARCH=x86_64;\n")
-            for bench in benchmarks:
-                f.write(f"cd {current_location}/{bench.upper()}/profiling && ./$(ls {bench}_aarch64_*.profiling);\n")
-                f2.write(f"cd {current_location}/{bench.upper()}/profiling/x86_64 && ./$(ls {bench}_x86_64_*.profiling);\n")
-                f3.write(f"echo diff {bench.upper()};\n")
-                f3.write(f"diff {current_location}/{bench.upper()}/profiling/profiler_output.txt {current_location}/{bench.upper()}/profiling/x86_64/profiler_output.txt;\n")
-
 with output_build_profiling_script.open("w") as base_script:
     with output_bash_aarch64_script.open("w") as arm_script:
         with output_bash_x86_64_script.open("w") as x86_script:
@@ -46,7 +32,7 @@ with output_build_profiling_script.open("w") as base_script:
                     arm_script.write(f"cd {current_location} && make final_compile_profiling PROGRAM={bench} SIZE={size} TARGET_ARCH=aarch64;\n")
                     x86_script.write(f"cd {current_location} && make final_compile_profiling PROGRAM={bench} SIZE={size} TARGET_ARCH=x86_64;\n")
                 for bench in benchmarks:
-                    arm_script.write(f"cd {current_location}/{bench.upper()}/profiling/aarch64 && ./$(ls {bench}_aarch64_*.profiling);\n")
-                    x86_script.write(f"cd {current_location}/{bench.upper()}/profiling/x86_64 && ./$(ls {bench}_x86_64_*.profiling);\n")
+                    arm_script.write(f"cd {current_location}/{bench.upper()}/profiling/{region_size}/aarch64 && ./$(ls {bench}_aarch64_*.profiling);\n")
+                    x86_script.write(f"cd {current_location}/{bench.upper()}/profiling/{region_size}/x86_64 && ./$(ls {bench}_x86_64_*.profiling);\n")
                     diff_f.write(f"echo diff {bench.upper()};\n")
-                    diff_f.write(f"diff {current_location}/{bench.upper()}/profiling/aarch64/profiler_output.txt {current_location}/{bench.upper()}/profiling/x86_64/profiler_output.txt;\n")
+                    diff_f.write(f"diff {current_location}/{bench.upper()}/profiling/{region_size}/aarch64/profiler_output.txt {current_location}/{bench.upper()}/profiling/{region_size}/x86_64/profiler_output.txt;\n")
