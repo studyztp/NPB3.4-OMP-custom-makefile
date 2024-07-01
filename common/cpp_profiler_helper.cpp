a5f8214bfeb0b98292ec4dab1e14b577f3f97407
studyztp <studyztp@gmail.com>
2024-07-01 09:44:45 -0700

    a line here before any further changes

--- common/cpp_profiler_helper.cpp
+++ common/cpp_profiler_helper.cpp
@@ -3,24 +3,17 @@
 #include <string>
 #include <omp.h>
 
-#ifdef ATOMIC
 #include <atomic>
 std::atomic<uint64_t> counter(0);
-#else
-uint64_t** counters;
-uint64_t current_counter;
-uint8_t num_threads;
-#endif
 
 omp_lock_t lock;
 bool wait = false;
 bool ifStart = false;
-uint64_t threshold = 100000000;
 uint64_t region = 0;
+uint64_t totalIRInst = 0;
 
 __attribute__((no_profile_instrument_function, noinline))
-void bb_hook(uint64_t inst) {
-#ifdef ATOMIC
+void bb_hook(uint64_t inst, uint64_t threshold) {
     if (ifStart) {
         if (wait) {
             omp_set_lock(&lock);
@@ -31,54 +24,22 @@ void bb_hook(uint64_t inst) {
             if (counter.load() >= threshold) {
                 omp_set_lock(&lock);
                 wait = true;
+                region ++;
+                totalIRInst += counter.load();
                 counter = 0;
-                region ++;
                 wait = false;
                 omp_unset_lock(&lock);
             }
         }
     }
-#else
-    if (ifStart) {
-        if (wait) {
-            omp_set_lock(&lock);
-            omp_unset_lock(&lock);
-        }
-        *counters[omp_get_thread_num()] += inst;
-        if (omp_get_thread_num() == 0) {
-            current_counter = *counters[0];
-            for (uint8_t i = 1; i < num_threads; i++) {
-                current_counter += *counters[i];
-            }
-            if (current_counter >= threshold) {
-                omp_set_lock(&lock);
-                wait = true;
-                for (uint8_t i = 0; i < num_threads; i++) {
-                    *counters[i] = 0;
-                }
-                region ++;
-                wait = false;
-                omp_unset_lock(&lock);
-            }
-        }
-    }
-#endif
 }
 
 __attribute__((no_profile_instrument_function))
 extern "C" void roi_begin_() {
-#ifdef ATOMIC
     counter = 0;
-#else
-    num_threads = omp_get_max_threads();
-    counters = (uint64_t**)malloc(num_threads * sizeof(uint64_t*));
-    for (int i = 0; i < num_threads; i++) {
-        counters[i] = (uint64_t*)malloc(sizeof(uint64_t));
-        *counters[i] = 0;
-    }
-#endif 
     omp_init_lock(&lock);
     ifStart = true;
+
     printf("ROI begin\n");
 }
 
@@ -86,13 +47,9 @@ __attribute__((no_profile_instrument_function))
 extern "C" void roi_end_() {
     ifStart = false;
     omp_destroy_lock(&lock);
-#ifdef PARALLEL
-    for (int i = 0; i < num_threads; i++) {
-        free(counters[i]);
-    }
-    free(counters);
-#endif
+
     printf("ROI end\n");
     printf("Region: %lu\n", region);
+    printf("Total IR instructions: %lu\n", totalIRInst);
 }
 
--- generate_timing_script.py
+++ generate_timing_script.py
@@ -4,13 +4,9 @@ benchmarks = ["bt", "cg", "ep", "ft", "is", "lu", "mg", "sp"]
 region_length = 100_000_000
 
 script = ""
-testing_script = ""
-size = "B"
-repeat_testing = 2
+sizes = ["B"]
 
 workdir = Path().cwd()
-
-testing_script += f"export LD_LIBRARY_PATH={workdir.as_posix()}/common/x86_64-unknown-linux-gnu:$LD_LIBRARY_PATH;\n"
 
 for bench in benchmarks:
     script += f"make clean_all_testing PROGRAM={bench};\n"
@@ -21,31 +17,3 @@ for bench in benchmarks:
     script += f"make cpp_parallel_profiling PROGRAM={bench} SIZE={size};\n"
     script += f"make c_atomic_profiling PROGRAM={bench} SIZE={size};\n"
     script += f"make c_parallel_profiling PROGRAM={bench} SIZE={size};\n"
-
-thread_num = 1
-
-while thread_num < 17:
-    testing_script += f"cd {workdir.as_posix()};\n"
-    testing_script += f"export OMP_NUM_THREADS={thread_num};\n"
-    
-    for bench in benchmarks:
-        testing_script += f"echo Testing {bench} for {thread_num} threads;\n"
-        bench_dir = Path(workdir / bench.upper())
-        for test_num in range(repeat_testing):
-            testing_script += f"echo start timing naive for {thread_num} threads for {test_num} times;\n"
-            testing_script += f"cd {bench_dir.as_posix()}/naive && (time ./*.naive) > timing_{thread_num}_threads_{test_num}.log 2>> timing_{thread_num}_threads_{test_num}.err;\n"
-            testing_script += f"echo start timing cpp_atomic_profiling for {thread_num} threads for {test_num} times;\n"
-            testing_script += f"cd {bench_dir.as_posix()}/cpp_atomic_profiling/{region_length} && (time ./*.cpp_atomic_profiling) > timing_{thread_num}_threads_{test_num}.log 2>> timing_{thread_num}_threads_{test_num}.err;\n"
-            testing_script += f"echo start timing cpp_parallel_profiling for {thread_num} threads for {test_num} times;\n"
-            testing_script += f"cd {bench_dir.as_posix()}/cpp_parallel_profiling/{region_length} && (time ./*.cpp_parallel_profiling) > timing_{thread_num}_threads_{test_num}.log 2>> timing_{thread_num}_threads_{test_num}.err;\n"
-            testing_script += f"echo start timing c_atomic_profiling for {thread_num} threads for {test_num} times;\n"
-            testing_script += f"cd {bench_dir.as_posix()}/c_atomic_profiling/{region_length} && (time ./*.c_atomic_profiling) > timing_{thread_num}_threads_{test_num}.log 2>> timing_{thread_num}_threads_{test_num}.err;\n"
-            testing_script += f"echo start timing c_parallel_profiling for {thread_num} threads for {test_num} times;\n"
-            testing_script += f"cd {bench_dir.as_posix()}/c_parallel_profiling/{region_length} && (time ./*.c_parallel_profiling) > timing_{thread_num}_threads_{test_num}.log 2>> timing_{thread_num}_threads_{test_num}.err;\n"
-    thread_num *= 2
-
-with open(workdir / "timing_script.sh", "w") as f:
-    f.write(script)
-
-with open(workdir / "testing_timing_script.sh", "w") as f:
-    f.write(testing_script)
